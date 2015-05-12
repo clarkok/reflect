a840e7497e527fbc5ec1c7c68cbec04979e8f130
Clarkok Zhang <mail@clarkokc.com>
2015-05-12 18:12:01 +0800

    enum reflection added

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Complete C++ reflect library in less than 200 lines code.
 
 # Basic Usage
+
+## Class reflection
+
 ```C++
 #include <iostream>
 #include "reflect.h"
@@ -42,6 +45,40 @@ Base
 Class1
 ```
 
+## Enum reflection
+
+```C++
+#include <iostream>
+#include "reflect-enum.h"
+
+ENUM_CLASS(Test,
+  TEST1,
+  TEST2,
+  TEST3
+);
+
+int main()
+{
+  std::cout << enum_reflection<Test>()->toString(Test::TEST1) << std::endl;
+
+  std::cout << enum_reflection<Test>()->toString(
+      enum_reflection<Test>()->fromString("Test::TEST1")
+    ) << std::endl;
+
+  std::cout << enum_reflection<Test>()->toString(
+      enum_reflection<Test>()->fromString("TEST1")
+    ) << std::endl;
+}
+```
+
+And the result:
+
+```
+Test::TEST1
+Test::TEST1
+Test::TEST1
+```
+
 # Document
 
 This library is a header-only library, include the header wherever you need.
@@ -50,13 +87,34 @@ This library supports multi-object-file projects, but has no effect on the
 efficiency of that program. Neither does this library need a non-standard
 preprocessor.
 
-## RE_class(ClassName, BaseClass [, tag])
+## Class reflection
+
+### RE_class(ClassName, BaseClass [, tag])
 Defined a class to reflect with in TAG. This macro is to replace 
 `class ClassName : public BaseClass`.
 
-## Reflect(BaseClass [, tag])
+### Reflect(BaseClass [, tag])
 Return a reference to a `map<string, factory>`, aka. the reflect map.
 
-## tag
+### tag
 A tag is a empty struct that defines a group of reflects, and each group of
 reflects will not affect others.
+
+## Enum reflection
+
+### ENUM_CLASS(EnumName, \<EnumMenbers\>)
+Defined a enum class to reflect, for example:
+
+```C++
+ENUM_CLASS( MyEnum    // enum class MyEnum {
+  Member1,            //   Member1,
+  Member2             //   Member2
+);                    // };
+```
+
+### std::string enum_reflection\<EnumName\>()->toString(EnumValue)
+map EnumValue to string
+
+### EnumName enum_reflection\<EnumName\>()->fromString(string)
+Reflect from string to EnumValue, Accept EnumMember with or without EnumName
+
---
+++ include/reflect/reflect-enum.h
@@ -0,0 +1,102 @@
+// Copyright (C) 2015 Clarkok Zhang
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef _CUA_DEBUG_REFLECT_ENUM_H_
+#define _CUA_DEBUG_REFLECT_ENUM_H_
+
+#include <string>
+#include <sstream>
+#include <map>
+#include <vector>
+
+template <typename T>
+class EnumReflectionHelper
+{
+    static T rand_result;
+public:
+    std::string toString(T) const {
+        return "";
+    }
+    T fromString(std::string) const {
+        return rand_result;
+    }
+};
+
+template <typename T>
+EnumReflectionHelper<T> *enum_reflection()
+{ return nullptr; }
+    
+#define __ENUM_TO_STRING(TypeName, ...)                         \
+    template<>                                                  \
+    class EnumReflectionHelper<TypeName>                        \
+    {                                                           \
+        static std::string                                      \
+        getName(std::stringstream &si)                          \
+        {                                                       \
+            std::string ret;                                    \
+            while (si &&                                        \
+                !((si.peek() >= 'a' && si.peek() <= 'z')||      \
+                (si.peek() >= 'A' && si.peek() <= 'Z')||        \
+                (si.peek() >= '0' && si.peek() <= '9')))        \
+                si.get();                                       \
+            if (!si) return "";                                 \
+            while (                                             \
+                (si.peek() >= 'a' && si.peek() <= 'z')||        \
+                (si.peek() >= 'A' && si.peek() <= 'Z')||        \
+                (si.peek() >= '0' && si.peek() <= '9'))         \
+                ret += si.get();                                \
+            return ret;                                         \
+        }                                                       \
+        std::vector<std::string> string_table;                  \
+        std::map<std::string, int> re_string_table;             \
+    public:                                                     \
+        EnumReflectionHelper()                                  \
+        {                                                       \
+            std::stringstream si(#__VA_ARGS__);                 \
+            std::string token;                                  \
+            while ((token = getName(si)).length()) {            \
+                string_table.push_back(#TypeName "::" + token); \
+                re_string_table[#TypeName "::" + token] =       \
+                re_string_table[token] = string_table.size() - 1;\
+            }                                                   \
+        }                                                       \
+        std::string toString(TypeName type) const               \
+        { return string_table[static_cast<int>(type)]; }        \
+        TypeName fromString(std::string s) const                \
+        {                                                       \
+            return static_cast<TypeName>(                       \
+                re_string_table.at(s));                         \
+        }                                                       \
+    };                                                          \
+    template<> EnumReflectionHelper<TypeName> *                 \
+    enum_reflection<TypeName>()                                 \
+    {                                                           \
+        static EnumReflectionHelper<TypeName> helper;           \
+        return &helper;                                         \
+    }
+    
+#define __ENUM_DEFINE(TypeName, ...)        \
+    enum class TypeName { __VA_ARGS__ };    \
+
+#define ENUM_CLASS(TypeName, ...)       \
+    __ENUM_DEFINE(TypeName, __VA_ARGS__)\
+    __ENUM_TO_STRING(TypeName, __VA_ARGS__)
+
+#endif // _CUA_DEBUG_REFLECT_ENUM_H_
